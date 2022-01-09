36aef2fd26df371d8929a8bdb59cb912192556ab
Gaspar <gpalaciostorre@gmail.com>
2022-01-09 16:37:12 -0300

    Desafío 1

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function NavBar(){
+    
+    return(
+        <>
+        <header>
+            <h1>Melody Clothes</h1>
+            <nav>
+                <ul>
+                    <li><a href="">Prendas</a></li>
+                    <li><a href="">Descuentos</a></li>
+                    <li><a href="">Contacto</a></li>
+                </ul>
+            </nav>
+        </header>
+        </>
+    )
+};
